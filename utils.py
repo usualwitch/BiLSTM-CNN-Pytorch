182e285010bca3d0ac62827b8191912fbbb28c3b
OrdinaryWitch <yw.chen.math@gmail.com>
2020-05-28 11:02:24 +0800

    Prepared data.

--- utils.py
+++ utils.py
@@ -9,20 +9,24 @@ from datetime import timedelta
 
 
 MAX_VOCAB_SIZE = 10000  # 词表长度限制
-UNK, PAD = '<UNK>', '<PAD>'  # 未知字，padding符号
+UNK, PAD = "<UNK>", "<PAD>"  # 未知字，padding符号
 
 
 def build_vocab(file_path, tokenizer, max_size, min_freq):
     vocab_dic = {}
-    with open(file_path, 'r', encoding='UTF-8') as f:
+    with open(file_path, "r", encoding="UTF-8") as f:
         for line in tqdm(f):
             lin = line.strip()
             if not lin:
                 continue
-            content = lin.split('\t')[0]
+            content = lin.split("\t")[0]
             for word in tokenizer(content):
                 vocab_dic[word] = vocab_dic.get(word, 0) + 1
-        vocab_list = sorted([_ for _ in vocab_dic.items() if _[1] >= min_freq], key=lambda x: x[1], reverse=True)[:max_size]
+        vocab_list = sorted(
+            [_ for _ in vocab_dic.items() if _[1] >= min_freq],
+            key=lambda x: x[1],
+            reverse=True,
+        )[:max_size]
         vocab_dic = {word_count[0]: idx for idx, word_count in enumerate(vocab_list)}
         vocab_dic.update({UNK: len(vocab_dic), PAD: len(vocab_dic) + 1})
     return vocab_dic
@@ -30,24 +34,26 @@ def build_vocab(file_path, tokenizer, max_size, min_freq):
 
 def build_dataset(config, ues_word):
     if ues_word:
-        tokenizer = lambda x: x.split(' ')  # 以空格隔开，word-level
+        tokenizer = lambda x: x.split(" ")  # 以空格隔开，word-level
     else:
         tokenizer = lambda x: [y for y in x]  # char-level
     if os.path.exists(config.vocab_path):
-        vocab = pkl.load(open(config.vocab_path, 'rb'))
+        vocab = pkl.load(open(config.vocab_path, "rb"))
     else:
-        vocab = build_vocab(config.train_path, tokenizer=tokenizer, max_size=MAX_VOCAB_SIZE, min_freq=1)
-        pkl.dump(vocab, open(config.vocab_path, 'wb'))
+        vocab = build_vocab(
+            config.train_path, tokenizer=tokenizer, max_size=MAX_VOCAB_SIZE, min_freq=1
+        )
+        pkl.dump(vocab, open(config.vocab_path, "wb"))
     print(f"Vocab size: {len(vocab)}")
 
     def load_dataset(path, pad_size=32):
         contents = []
-        with open(path, 'r', encoding='UTF-8') as f:
+        with open(path, "r", encoding="UTF-8") as f:
             for line in tqdm(f):
                 lin = line.strip()
                 if not lin:
                     continue
-                content, label = lin.split('\t')
+                content, label = lin.split("\t")
                 words_line = []
                 token = tokenizer(content)
                 seq_len = len(token)
@@ -62,6 +68,7 @@ def build_dataset(config, ues_word):
                     words_line.append(vocab.get(word, vocab.get(UNK)))
                 contents.append((words_line, int(label), seq_len))
         return contents  # [([...], 0), ([...], 1), ...]
+
     train = load_dataset(config.train_path, config.pad_size)
     dev = load_dataset(config.dev_path, config.pad_size)
     test = load_dataset(config.test_path, config.pad_size)
@@ -89,7 +96,7 @@ class DatasetIterater(object):
 
     def __next__(self):
         if self.residue and self.index == self.n_batches:
-            batches = self.batches[self.index * self.batch_size: len(self.batches)]
+            batches = self.batches[self.index * self.batch_size : len(self.batches)]
             self.index += 1
             batches = self._to_tensor(batches)
             return batches
@@ -98,7 +105,9 @@ class DatasetIterater(object):
             self.index = 0
             raise StopIteration
         else:
-            batches = self.batches[self.index * self.batch_size: (self.index + 1) * self.batch_size]
+            batches = self.batches[
+                self.index * self.batch_size : (self.index + 1) * self.batch_size
+            ]
             self.index += 1
             batches = self._to_tensor(batches)
             return batches
@@ -126,7 +135,7 @@ def get_time_dif(start_time):
 
 
 if __name__ == "__main__":
-    '''提取预训练词向量'''
+    """提取预训练词向量"""
     # 下面的目录、文件名按需更改。
     train_dir = "./THUCNews/data/train.txt"
     vocab_dir = "./THUCNews/data/vocab.pkl"
@@ -134,15 +143,17 @@ if __name__ == "__main__":
     emb_dim = 300
     filename_trimmed_dir = "./THUCNews/data/embedding_SougouNews"
     if os.path.exists(vocab_dir):
-        word_to_id = pkl.load(open(vocab_dir, 'rb'))
+        word_to_id = pkl.load(open(vocab_dir, "rb"))
     else:
         # tokenizer = lambda x: x.split(' ')  # 以词为单位构建词表(数据集中词之间以空格隔开)
         tokenizer = lambda x: [y for y in x]  # 以字为单位构建词表
-        word_to_id = build_vocab(train_dir, tokenizer=tokenizer, max_size=MAX_VOCAB_SIZE, min_freq=1)
-        pkl.dump(word_to_id, open(vocab_dir, 'wb'))
+        word_to_id = build_vocab(
+            train_dir, tokenizer=tokenizer, max_size=MAX_VOCAB_SIZE, min_freq=1
+        )
+        pkl.dump(word_to_id, open(vocab_dir, "wb"))
 
     embeddings = np.random.rand(len(word_to_id), emb_dim)
-    f = open(pretrain_dir, "r", encoding='UTF-8')
+    f = open(pretrain_dir, "r", encoding="UTF-8")
     for i, line in enumerate(f.readlines()):
         # if i == 0:  # 若第一行是标题，则跳过
         #     continue
@@ -150,6 +161,6 @@ if __name__ == "__main__":
         if lin[0] in word_to_id:
             idx = word_to_id[lin[0]]
             emb = [float(x) for x in lin[1:301]]
-            embeddings[idx] = np.asarray(emb, dtype='float32')
+            embeddings[idx] = np.asarray(emb, dtype="float32")
     f.close()
     np.savez_compressed(filename_trimmed_dir, embeddings=embeddings)
